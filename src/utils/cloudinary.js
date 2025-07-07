45ec9ff781474392c12bd9c15f243e1dbcd99f86
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2025-07-07 15:08:00 +0530

    making some changes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,9 +1,9 @@
 import {v2 as cloudinary} from 'cloudinary'
 import fs from 'fs'
   cloudinary.config({ 
-        cloud_name: process.env.api_secret, 
-        api_key: process.env.api_key , 
-        api_secret: 'eso6ca4PPXwTm14ya_uxvV1_Ad0'
+        cloud_name: process.env.Cloud_name, 
+        api_key: process.env.Api_key , 
+        api_secret: process.env.Api_secret
 
 // ' // Click 'View API Keys' above to copy your API secret
     });
