229f5ef80b40d3eb6a4dbec932997f76b21904cd
dhruvmohan867 <dhruvmohanshukla@gmail.com>
2025-07-07 20:21:12 +0530

    error found

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose'
 import express from 'express'
 import { DB_NAME } from './constants.js';
 import connectDB from './db/index.js';
- 
-
+const app = express()
+const PORT = process.env.PORT
 connectDB()
 .then(()=>{
-     app.listen(process.env.PORT || 8000, ()=>{
+     app.listen(PORT || 8000, ()=>{
        console.log(`Server is running at port :${process.env.PORT}`)
      })
 })
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,22 @@ import fs from 'fs'
 
 // ' // Click 'View API Keys' above to copy your API secret
     });
+
+const uploadCloudinary = async(localFilePath)=>{
+   try{
+     if (!localFilePath){
+      return null;
+    }
+     const response = await cloudinary.uploader.upload(localFilePath,{
+         resource_type : "auto"
+     })
+     console.log("File upload ho gyi hai")
+     console.log(response.url)
+     return response;
+   }
+   catch(error){
+      fs.unlink(localFilePath)
+      return null
+   }
+}
+export {uploadCloudinary}
